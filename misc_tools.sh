593e1cf49bcf8ea96832f4e93ac1e5bb88ca984b
Amin <ameen.yaseen.98@gmail.com>
2025-06-04 09:15:03 +0200

    Create misc_tools.sh

---
+++ misc_tools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# rustscan
+if ! command -v rustscan &>/dev/null; then
+  echo "[+] Installing rustscan..."
+  RUSTSCAN_URL=$(curl -s https://api.github.com/repos/RustScan/RustScan/releases/latest | grep browser_download_url | grep amd64.deb | cut -d '"' -f 4)
+  wget "$RUSTSCAN_URL" -O rustscan.deb
+  sudo dpkg -i rustscan.deb || sudo apt -f install -y
+  rm rustscan.deb
+fi
