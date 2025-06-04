57ba9f884b411a3b0916ad97862c0bf1ecfa4563
Amin <ameen.yaseen.98@gmail.com>
2025-06-04 21:06:22 +0200

    Create AD_pkgs.sh

---
+++ AD_pkgs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Install dependencies
+sudo apt update && sudo apt install -y golang mitm6 impacket-scripts
+
+# Clone kerbrute repo
+git clone https://github.com/ropnop/kerbrute.git ~/Downloads/kerbrute
+# Build kerbrute
+cd ~/Downloads/kerbrute
+make linux
+# Move binary to PATH
+sudo mv dist/kerbrute_linux_amd64 /usr/local/bin/kerbrute
+sudo chmod +x /usr/local/bin/kerbrute
+
+echo "[✓] kerbrute installed to /usr/local/bin/kerbrute"
