bc5657d8f6d6827bf068eaf777e54bed16ccb569
Amin <ameen.yaseen.98@gmail.com>
2025-06-08 10:53:43 +0200

    Remove unnecessary pkgs

--- installation_scripts/AD_tools.sh
+++ installation_scripts/AD_tools.sh
@@ -3,7 +3,7 @@ set -e
 
 # ---------- SYSTEM DEPENDENCIES ----------
 echo "[*] Installing mitm6, Impacket, Go ..."
-sudo apt update && sudo apt install -y golang mitm6 impacket-scripts unzip wget make python3-pip bloodhound
+sudo apt update && sudo apt install -y golang mitm6 impacket-scripts python3-pip bloodhound
 echo "[✓] Core dependencies installed"
 
 # ---------- KERBRUTE ----------
